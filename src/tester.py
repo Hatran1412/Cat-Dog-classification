1db639137f0af6689f4090a75c15f749f5e4d232
Hatran1412 <tran.ha.14122000@gmail.com>
2021-09-02 20:45:20 +0700

    define function tester

---
+++ src/tester.py
@@ -0,0 +1,24 @@
+
+import torch
+import torch.nn.functional as F
+from src.loadingdata import test_loader
+from src.loadingdata import accuracy_list
+
+
+def test(model):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            predict = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += predict.eq(target.data.view_as(predict)).cpu().sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100. * correct / len(test_loader.dataset)
+    accuracy_list.append(accuracy)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        accuracy))
